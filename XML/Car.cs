20bd33b12a94f69c2b65849297d567c078939e4c
Kostya Popov <kepel2823@gmail.com>
2023-12-03 23:17:07 +0200

    Add project files.

---
+++ XML/Car.cs
@@ -0,0 +1,18 @@
+﻿namespace XML
+{
+    public class Car
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Number { get; set; }
+
+        public Car(string name, decimal price, string number)
+        {
+            Name = name;
+            Price = price;
+            Number = number;
+        }
+    }
+}
---
+++ XML/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Xml;
+using System.Xml.Linq;
+
+namespace XML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = new List<Car>
+            {
+                new Car("Toyota", 25000m, "123AX"),
+                new Car("BMW", 35000m, "456AS"),
+                new Car("Honda", 20000m, "789RF")
+            };
+
+            SaveCollectionInXmlFile(cars);
+            SetNewCarPriceByNumber("123AX", 44000m);
+        }
+
+        static void SaveCollectionInXmlFile(IEnumerable<Car> cars)
+        {
+            var xmlCars = new XElement("Cars",
+            cars.Select(car => new XElement("Car",
+                new XElement("Name", car.Name),
+                new XElement("Price", car.Price),
+                new XElement("Number", car.Number))));
+
+            xmlCars.Save("Xml.xml");
+            Console.WriteLine("Collection saved in Xml.xml");
+        }
+
+        static void SetNewCarPriceByNumber(string carNumber, decimal newPrice)
+        {
+            var carsXml = XElement.Load("Xml.xml");
+            var carToUpdate = carsXml.Elements("Car").FirstOrDefault(car => car.Element("Number")?.Value.Equals(carNumber) ?? false);
+
+            if (carToUpdate is not null)
+            {
+                var priceElement = carToUpdate.Element("Price");
+                if (priceElement is not null)
+                {
+                    priceElement.Value = newPrice.ToString();
+                    carsXml.Save("Xml.xml");
+                    Console.WriteLine($"Price for car with number {carNumber} updated to {newPrice}");
+                }
+                else
+                {
+                    Console.WriteLine($"Price element for car with number {carNumber} not found");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Car with number {carNumber} not found");
+            }
+        }
+    }
+}
